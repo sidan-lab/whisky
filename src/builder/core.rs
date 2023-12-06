8a986e98a94b965967f142e5da22b4d5d57c9ae6
TW <tszwai@emurgo.io>
2023-12-06 16:00:59 +0800

    add tx builder impl

--- src/builder/core.rs
+++ src/builder/core.rs
@@ -1,14 +1,42 @@
-use crate::builder::models::*;
+use cardano_serialization_lib::tx_builder::TransactionBuilder;
+
+use crate::{builder::models::*, utils::csl::build_tx_builder};
 
 pub struct MeshTxBuilderCore {
-    mesh_tx_builder_body: MeshTxBuilderBody
+    tx_builder: TransactionBuilder,
+    mesh_tx_builder_body: MeshTxBuilderBody,
 }
 
 impl MeshTxBuilderCore {
-    pub fn complete_sync(
-        self,
-        customized_tx: Option<MeshTxBuilderBody>
-    ) -> MeshTxBuilderCore {
+    pub fn new() -> Self {
+        Self {
+            tx_builder: build_tx_builder(),
+            mesh_tx_builder_body: MeshTxBuilderBody {
+                inputs: vec![],
+                outputs: vec![],
+                collaterals: vec![],
+                required_signatures: vec![],
+                reference_inputs: vec![],
+                mints: vec![],
+                change_address: String::from(""),
+                metadata: vec![],
+                validity_range: ValidityRange {
+                    invalid_before: None,
+                    invalid_hereafter: None,
+                },
+                signing_key: vec![],
+            },
+        }
+    }
+
+    pub fn complete_sync(mut self, customized_tx: Option<MeshTxBuilderBody>) -> MeshTxBuilderCore {
+        if customized_tx.is_some() {
+            self.mesh_tx_builder_body = customized_tx.unwrap();
+        }
+        self
+    }
+
+    pub fn serialize_tx_body(self, tx_body: MeshTxBuilderBody) -> MeshTxBuilderCore {
         self
     }
 }
--- src/utils/csl.rs
+++ src/utils/csl.rs
@@ -1,1 +1,24 @@
+use cardano_serialization_lib::{
+    fees::LinearFee, plutus::ExUnitPrices, tx_builder::*, utils::*, UnitInterval,
+};
 
+pub fn to_bignum(val: u64) -> BigNum {
+    BigNum::from_str(&val.to_string()).unwrap()
+}
+
+pub fn build_tx_builder() -> TransactionBuilder {
+    let cfg = TransactionBuilderConfigBuilder::new()
+        .fee_algo(&LinearFee::new(&to_bignum(44), &to_bignum(155381)))
+        .pool_deposit(&to_bignum(500000000))
+        .key_deposit(&to_bignum(2000000))
+        .max_value_size(5000)
+        .max_tx_size(16384)
+        .coins_per_utxo_byte(&to_bignum(4310))
+        .ex_unit_prices(&ExUnitPrices::new(
+            &UnitInterval::new(&to_bignum(577), &to_bignum(10000)),
+            &UnitInterval::new(&to_bignum(721), &to_bignum(10000000)),
+        ))
+        .build()
+        .unwrap();
+    return TransactionBuilder::new(&cfg);
+}
