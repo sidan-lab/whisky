1ad3c015fd33731b95c985209505e767f37ddfa2
TW <tszwai@emurgo.io>
2023-12-06 14:06:52 +0800

    initial type signature for MeshTxBuilderCore

--- src/builder/core.rs
+++ src/builder/core.rs
@@ -1,1 +1,14 @@
+use crate::builder::models::*;
 
+pub struct MeshTxBuilderCore {
+    mesh_tx_builder_body: MeshTxBuilderBody
+}
+
+impl MeshTxBuilderCore {
+    pub fn complete_sync(
+        self,
+        customized_tx: Option<MeshTxBuilderBody>
+    ) -> MeshTxBuilderCore {
+        self
+    }
+}
