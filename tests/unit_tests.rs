a913e7304e0b16ddbe390b95bc440049420442f2
twwu123 <tszwaiwu.96@gmail.com>
2024-01-05 16:00:19 +0800

    test script tx in

--- tests/unit_tests.rs
+++ tests/unit_tests.rs
@@ -1,5 +1,9 @@
 mod tests {
-    use sidan_csl_rs::builder::{core::MeshTxBuilderCore, models::Asset};
+    use serde_json::{json, to_string};
+    use sidan_csl_rs::builder::{
+        core::MeshTxBuilderCore,
+        models::{Asset, Budget, Redeemer},
+    };
 
     #[test]
     fn test_mesh_tx_builder_core() {
@@ -13,6 +17,42 @@ mod tests {
             unit: "lovelace".to_string(),
             quantity: "30000000".to_string(),
         };
-        mesh.tx_in("93fec6deaafabcc394a15552b57b1beca120d9ee90480d1e5cb42ff20118d40a".to_string(), 1, vec![asset], "addr_test1vr3vljjxan0hl6u28fle2l4ds6ugc9t08lwevpauk38t3agx7rtq6".to_string());
+        mesh.tx_in(
+            "93fec6deaafabcc394a15552b57b1beca120d9ee90480d1e5cb42ff20118d40a".to_string(),
+            1,
+            vec![asset],
+            "addr_test1vr3vljjxan0hl6u28fle2l4ds6ugc9t08lwevpauk38t3agx7rtq6".to_string(),
+        );
+    }
+
+    #[test]
+    fn test_script_tx_in() {
+        let mut mesh = MeshTxBuilderCore::new();
+        let asset = Asset {
+            unit: "lovelace".to_string(),
+            quantity: "30000000".to_string(),
+        };
+
+        let data = to_string(&json!({
+            "constructor": 0,
+            "fields": []
+        }))
+        .unwrap();
+
+        mesh.spending_plutus_script_v2()
+            .tx_in(
+                "93fec6deaafabcc394a15552b57b1beca120d9ee90480d1e5cb42ff20118d40a".to_string(),
+                1,
+                vec![asset],
+                "addr_test1vr3vljjxan0hl6u28fle2l4ds6ugc9t08lwevpauk38t3agx7rtq6".to_string(),
+            )
+            .spending_reference_tx_in_inline_datum_present()
+            .spending_reference_tx_in_redeemer_value(Redeemer {
+                data,
+                ex_units: Budget {
+                    mem: 3386819,
+                    steps: 1048170931,
+                },
+            });
     }
 }
