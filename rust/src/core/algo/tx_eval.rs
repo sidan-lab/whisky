ea49acc86daef32484537a7b599f3936c035099e
twwu123 <tszwaiwu.96@gmail.com>
2024-04-12 18:24:33 +0800

    implement tx eval

--- rust/src/core/algo/tx_eval.rs
+++ rust/src/core/algo/tx_eval.rs
@@ -1,13 +1,15 @@
 use cardano_serialization_lib as csl;
+use pallas::ledger::primitives::alonzo::Redeemer;
 use pallas::ledger::primitives::conway::PlutusV2Script;
 use std::collections::HashMap;
+use uplc::tx::SlotConfig;
 use uplc::Fragment;
 
 use crate::model::{Asset, UTxO, UtxoOutput};
 use cardano_serialization_lib::address::Address;
 use pallas::codec::utils::{Bytes, CborWrap, KeyValuePairs};
 use pallas::ledger::primitives::babbage::{
-    AssetName, Coin, DatumOption, Multiasset, PlutusData, PolicyId, PostAlonzoTransactionOutput,
+    AssetName, Coin, CostMdls, DatumOption, PlutusData, PolicyId, PostAlonzoTransactionOutput,
     PseudoScript, ScriptRef, TransactionOutput, Value,
 };
 use pallas::ledger::traverse::{Era, MultiEraTx};
@@ -16,35 +18,71 @@ use uplc::{
     Hash, TransactionInput,
 };
 
-// pub fn tx_eval(tx_hex: &str, inputs: &Vec<UTxO>) -> Result<&str, String> {
-//     let tx_bytes = hex::decode(tx_hex).expect("Invalid tx hex");
-//     let mtx = MultiEraTx::decode_for_era(Era::Babbage, &tx_bytes);
-//     let tx = match mtx {
-//         Ok(MultiEraTx::Babbage(tx)) => tx.into_owned(),
-//         _ => return Err("Invalid Tx Era".to_string()),
-//     };
+pub fn tx_eval(tx_hex: &str, inputs: &Vec<UTxO>) -> Result<Vec<Redeemer>, String> {
+    let tx_bytes = hex::decode(tx_hex).expect("Invalid tx hex");
+    let mtx = MultiEraTx::decode_for_era(Era::Babbage, &tx_bytes);
+    let tx = match mtx {
+        Ok(MultiEraTx::Babbage(tx)) => tx.into_owned(),
+        _ => return Err("Invalid Tx Era".to_string()),
+    };
 
-//     eval_phase_two(
-//         &tx,
-//         utxos,
-//         cost_mdls,
-//         initial_budget,
-//         slot_config,
-//         true,
-//         with_redeemer,
-//     );
-//     Ok("")
-// }
+    eval_phase_two(
+        &tx,
+        &to_pallas_utxos(inputs)?,
+        Some(&get_cost_mdls()),
+        None,
+        &SlotConfig::default(),
+        false,
+        |_r| (),
+    )
+    .map_err(|err| format!("Error occurred during evaluation: {}", err))
+}
 
-fn to_pallas_utxo(utxos: &Vec<UTxO>) -> Result<Vec<ResolvedInput>, String> {
+fn get_cost_mdls() -> CostMdls {
+    let plutus_v1_cost_model: Vec<i64> = vec![
+        205665, 812, 1, 1, 1000, 571, 0, 1, 1000, 24177, 4, 1, 1000, 32, 117366, 10475, 4, 23000,
+        100, 23000, 100, 23000, 100, 23000, 100, 23000, 100, 23000, 100, 100, 100, 23000, 100,
+        19537, 32, 175354, 32, 46417, 4, 221973, 511, 0, 1, 89141, 32, 497525, 14068, 4, 2, 196500,
+        453240, 220, 0, 1, 1, 1000, 28662, 4, 2, 245000, 216773, 62, 1, 1060367, 12586, 1, 208512,
+        421, 1, 187000, 1000, 52998, 1, 80436, 32, 43249, 32, 1000, 32, 80556, 1, 57667, 4, 1000,
+        10, 197145, 156, 1, 197145, 156, 1, 204924, 473, 1, 208896, 511, 1, 52467, 32, 64832, 32,
+        65493, 32, 22558, 32, 16563, 32, 76511, 32, 196500, 453240, 220, 0, 1, 1, 69522, 11687, 0,
+        1, 60091, 32, 196500, 453240, 220, 0, 1, 1, 196500, 453240, 220, 0, 1, 1, 806990, 30482, 4,
+        1927926, 82523, 4, 265318, 0, 4, 0, 85931, 32, 205665, 812, 1, 1, 41182, 32, 212342, 32,
+        31220, 32, 32696, 32, 43357, 32, 32247, 32, 38314, 32, 9462713, 1021, 10,
+    ];
+
+    let plutus_v2_cost_model: Vec<i64> = vec![
+        205665, 812, 1, 1, 1000, 571, 0, 1, 1000, 24177, 4, 1, 1000, 32, 117366, 10475, 4, 23000,
+        100, 23000, 100, 23000, 100, 23000, 100, 23000, 100, 23000, 100, 100, 100, 23000, 100,
+        19537, 32, 175354, 32, 46417, 4, 221973, 511, 0, 1, 89141, 32, 497525, 14068, 4, 2, 196500,
+        453240, 220, 0, 1, 1, 1000, 28662, 4, 2, 245000, 216773, 62, 1, 1060367, 12586, 1, 208512,
+        421, 1, 187000, 1000, 52998, 1, 80436, 32, 43249, 32, 1000, 32, 80556, 1, 57667, 4, 1000,
+        10, 197145, 156, 1, 197145, 156, 1, 204924, 473, 1, 208896, 511, 1, 52467, 32, 64832, 32,
+        65493, 32, 22558, 32, 16563, 32, 76511, 32, 196500, 453240, 220, 0, 1, 1, 69522, 11687, 0,
+        1, 60091, 32, 196500, 453240, 220, 0, 1, 1, 196500, 453240, 220, 0, 1, 1, 1159724, 392670,
+        0, 2, 806990, 30482, 4, 1927926, 82523, 4, 265318, 0, 4, 0, 85931, 32, 205665, 812, 1, 1,
+        41182, 32, 212342, 32, 31220, 32, 32696, 32, 43357, 32, 32247, 32, 38314, 32, 20000000000,
+        20000000000, 9462713, 1021, 10, 20000000000, 0, 20000000000,
+    ];
+
+    CostMdls {
+        plutus_v1: Some(plutus_v1_cost_model),
+        plutus_v2: Some(plutus_v2_cost_model),
+    }
+}
+
+fn to_pallas_utxos(utxos: &Vec<UTxO>) -> Result<Vec<ResolvedInput>, String> {
     let mut resolved_inputs = Vec::new();
     for utxo in utxos {
-        let mut resolved_input: ResolvedInput;
+        let tx_hash: [u8; 32] = hex::decode(&utxo.input.tx_hash)
+            .map_err(|err| format!("Invalid tx hash found: {}", err))?
+            .try_into()
+            .map_err(|_e| format!("Invalid tx hash length found"))?;
+
         let resolved_input = ResolvedInput {
             input: TransactionInput {
-                transaction_id: Hash::from(
-                    hex::decode(utxo.input.tx_hash).unwrap().try_into().unwrap(),
-                ),
+                transaction_id: Hash::from(tx_hash),
                 index: utxo.input.output_index.try_into().unwrap(),
             },
             output: TransactionOutput::PostAlonzo(PostAlonzoTransactionOutput {
@@ -58,6 +96,7 @@ fn to_pallas_utxo(utxos: &Vec<UTxO>) -> Result<Vec<ResolvedInput>, String> {
                 script_ref: to_pallas_script_ref(&utxo.output)?,
             }),
         };
+        resolved_inputs.push(resolved_input);
     }
     Ok(resolved_inputs)
 }
