93d9eef0851b7a14446bd0dd57e394597ef8c19a
SIDANWhatever <wongkahinhinson@gmail.com>
2024-09-04 16:13:06 +0800

    docs: restructure examples

---
+++ example-web/next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: function (config, options) {
+    config.experiments = {
+      asyncWebAssembly: true,
+      layers: true,
+    };
+    return config;
+  },
+  pageExtensions: ["js", "jsx", "ts", "tsx"],
+};
+
+export default nextConfig;
