c53ca718b9aecba8a7f681b12d70d6c086784dbf
twwu123 <tszwaiwu.96@gmail.com>
2024-09-12 15:45:08 +0800

    add vote delegation test

--- packages/whisky/tests/integration_tests.rs
+++ packages/whisky/tests/integration_tests.rs
@@ -6,7 +6,7 @@ mod int_tests {
     };
     use whisky::{
         builder::{ TxBuilder, TxBuilderParam, WData::{self, JSON}, WRedeemer},
-        core::utils::merge_vkey_witnesses_to_transaction, model::Anchor,
+        core::utils::merge_vkey_witnesses_to_transaction, model::{Anchor, DRep},
     };
 
     #[test]
@@ -517,6 +517,33 @@ mod int_tests {
                 anchor_url: "https://raw.githubusercontent.com/HinsonSIDAN/cardano-drep/main/HinsonSIDAN.jsonld".to_string(),
                 anchor_data_hash: "2aef51273a566e529a2d5958d981d7f0b3c7224fc2853b6c4922e019657b5060".to_string()
             }))
+            .complete_sync(None)
+            .unwrap()
+            .complete_signing()
+            .unwrap();
+
+        println!("{}", unsigned_tx);
+    }
+
+    #[test]
+    fn test_vote_delegation() {
+        let mut mesh = TxBuilder::new(TxBuilderParam {
+            evaluator: None,
+            fetcher: None,
+            submitter: None,
+            params: None,
+        });
+
+
+        let unsigned_tx = mesh
+            .change_address("addr_test1qpsmz8q2xj43wg597pnpp0ffnlvr8fpfydff0wcsyzqyrxguk5v6wzdvfjyy8q5ysrh8wdxg9h0u4ncse4cxhd7qhqjqk8pse6")
+            .tx_in(
+                "2cb57168ee66b68bd04a0d595060b546edf30c04ae1031b883c9ac797967dd85",
+                3,
+                &[Asset::new_from_str("lovelace", "9891607895")],
+                "addr_test1vru4e2un2tq50q4rv6qzk7t8w34gjdtw3y2uzuqxzj0ldrqqactxh",
+            )
+            .vote_delegation_certificate("stake_test1uzdx8vwxvz5wy45fwdrwk2l85ax7j5wtr4cee6a8xc632cc3p6psh", &DRep::DRepId("drep1j6257gz2swty9ut46lspyvujkt02pd82am2zq97p7p9pv2euzs7".to_string()))
             .complete_sync(None)
             .unwrap()
             .complete_signing()
