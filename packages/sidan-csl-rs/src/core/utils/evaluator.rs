05a0be9a249cdadd137b0bc75e7355959a1ab8e8
lisicky <lisicky@inbox.ru>
2024-10-14 03:37:30 +0800

    add error text

--- packages/sidan-csl-rs/src/core/utils/evaluator.rs
+++ packages/sidan-csl-rs/src/core/utils/evaluator.rs
@@ -43,7 +43,7 @@ pub fn evaluate_tx_scripts_js(
     let deserialize_network = match serde_json::from_str(network.as_str()) {
         Ok(network) => network,
         Err(e) => {
-            return WasmResult::new_error("failure".to_string(), format!("{:?}", e));
+            return WasmResult::new_error("failure".to_string(), format!("Error in decoding network: {:?}", e));
         }
     };
 
