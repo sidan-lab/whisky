4c137b6fdacb633ab2596a776d48f2cdf8ee96a0
twwu123 <tszwaiwu.96@gmail.com>
2025-01-30 18:26:38 +0800

    bump csl

--- packages/whisky/tests/integration_tests.rs
+++ packages/whisky/tests/integration_tests.rs
@@ -528,6 +528,36 @@ mod int_tests {
     }
 
     #[test]
+    fn test_register_drep_cip129() {
+        let mut mesh = TxBuilder::new(TxBuilderParam {
+            evaluator: None,
+            fetcher: None,
+            submitter: None,
+            params: None,
+        });
+
+        let unsigned_tx = mesh
+            .change_address("addr_test1qpsmz8q2xj43wg597pnpp0ffnlvr8fpfydff0wcsyzqyrxguk5v6wzdvfjyy8q5ysrh8wdxg9h0u4ncse4cxhd7qhqjqk8pse6")
+            .tx_in(
+                "2cb57168ee66b68bd04a0d595060b546edf30c04ae1031b883c9ac797967dd85",
+                3,
+                &[Asset::new_from_str("lovelace", "9891607895")],
+                "addr_test1vru4e2un2tq50q4rv6qzk7t8w34gjdtw3y2uzuqxzj0ldrqqactxh",
+            )
+            .drep_registration("drep1y2tf2neqf2pevsh3wht7qy3nj2edag95athdggqhc8cy59s6skxy4", 500000000, Some(Anchor {
+                anchor_url: "https://raw.githubusercontent.com/HinsonSIDAN/cardano-drep/main/HinsonSIDAN.jsonld".to_string(),
+                anchor_data_hash: "2aef51273a566e529a2d5958d981d7f0b3c7224fc2853b6c4922e019657b5060".to_string()
+            }))
+            .complete_sync(None)
+            .unwrap()
+            .complete_signing()
+            .unwrap();
+
+        println!("{}", unsigned_tx);
+        assert!(mesh.core.mesh_csl.tx_hex != *"");
+    }
+
+    #[test]
     fn test_vote_delegation() {
         let mut mesh = TxBuilder::new(TxBuilderParam {
             evaluator: None,
