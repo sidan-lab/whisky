173363d31fefdd5d88e362f6124959a15fcbeec1
twwu123 <tszwaiwu.96@gmail.com>
2025-09-30 16:02:34 +0800

    fix wasm issues

--- packages/whisky-csl/src/utils/validator.rs
+++ packages/whisky-csl/src/utils/validator.rs
@@ -1,4 +1,4 @@
-use cardano_serialization_lib::JsError;
+use cquisitor_lib::js_error::JsError;
 use cquisitor_lib::validators::input_contexts::ValidationInputContext;
 use cquisitor_lib::validators::validation_result::ValidationResult;
 use cquisitor_lib::validators::validator::validate_transaction;
@@ -7,5 +7,5 @@ pub fn validate_tx(
     tx_hex: &str,
     validation_context: ValidationInputContext,
 ) -> Result<ValidationResult, JsError> {
-    validate_transaction(tx_hex, validation_context).map_err(|e| JsError::from_str(&e.to_string()))
+    validate_transaction(tx_hex, validation_context)
 }
--- packages/whisky-js/src/wasm/tx_validator.rs
+++ packages/whisky-js/src/wasm/tx_validator.rs
@@ -1,14 +1,7 @@
 use crate::*;
-use cquisitor_lib::validators::validator::validate_transaction_js;
+use cquisitor_lib::{js_error::JsError, validators::validator::validate_transaction_js};
 
 #[wasm_bindgen]
-pub fn js_validate_tx(tx_hex: String, validation_context: String) -> WasmResult {
-    let eval_result = validate_transaction_js(&tx_hex, &validation_context);
-    match eval_result {
-        Ok(result) => {
-            let result_json = serde_json::to_string(&result).unwrap();
-            WasmResult::new("success".to_string(), result_json)
-        }
-        Err(e) => WasmResult::new_error("failure".to_string(), format!("{:?}", e.to_string())),
-    }
+pub fn js_validate_tx(tx_hex: String, validation_context: String) -> Result<String, JsError> {
+    validate_transaction_js(&tx_hex, &validation_context)
 }
