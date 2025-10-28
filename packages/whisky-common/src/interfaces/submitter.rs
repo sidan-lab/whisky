a7acaac07a09421eab5b2f441f1eb0087956c02c
SIDANWhatever <wongkahinhinson@gmail.com>
2025-10-28 12:31:28 +0000

    fix: submitter trait

--- packages/whisky-common/src/interfaces/submitter.rs
+++ packages/whisky-common/src/interfaces/submitter.rs
@@ -2,6 +2,6 @@ use crate::*;
 use async_trait::async_trait;
 
 #[async_trait]
-pub trait Submitter: Send {
+pub trait Submitter: Send + Sync {
     async fn submit_tx(&self, tx_hex: &str) -> Result<String, WError>;
 }
