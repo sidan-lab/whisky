e83d15eaac7ecc9555fbf98b0b4d276fbc098bcc
twwu123 <tszwaiwu.96@gmail.com>
2026-01-02 16:36:03 +0800

    add test for mint

--- packages/whisky-pallas/tests/tx_test.rs
+++ packages/whisky-pallas/tests/tx_test.rs
@@ -1,44 +1,48 @@
 use whisky_common::{
-    Asset, Budget, Datum, DatumSource, InlineDatumSource, Output, OutputScriptSource, ProvidedScriptSource, PubKeyTxIn, Redeemer, ScriptTxIn, ScriptTxInParameter, TxBuilderBody, TxIn, TxInParameter, ValidityRange
+    Asset, Budget, Datum, DatumSource, InlineDatumSource, MintItem, MintParameter, Output,
+    ProvidedScriptSource, PubKeyTxIn, Redeemer, ScriptMint, ScriptSource, ScriptTxIn,
+    ScriptTxInParameter, TxBuilderBody, TxIn, TxInParameter, ValidityRange,
 };
-use whisky_pallas::{tx_builder::core_pallas::CorePallas, wrapper::transaction_body::StakeCredential, wrapper::transaction_body::DRep};
+use whisky_pallas::{
+    tx_builder::core_pallas::CorePallas, wrapper::transaction_body::DRep,
+    wrapper::transaction_body::StakeCredential,
+};
 
 #[test]
 fn test_from_tx_builder_body() {
     let tx_builder_body = TxBuilderBody {
-        inputs: vec![TxIn::PubKeyTxIn(PubKeyTxIn {
-            tx_in: TxInParameter {
-                tx_hash: "db0937db0e8a743e6e97e8cf29077af1e951b52e46f2e2c63ef12a3abaaf9052".to_string(),
-                tx_index: 0,
-                amount: Some(vec![Asset::new_from_str("lovelace", "4633697637")]),
-                address: Some("addr_test1qzjhvr7xdqmyk6x7ax84rtgs3uasqyrvglz4k08kwhw4q4jp2fnzs02hl5fhjdtw07kkxeyfac0gf9aepnpp4vv3yy2s67j7tj".to_string())
-            }
-        }), 
-        TxIn::ScriptTxIn(ScriptTxIn {
-            tx_in: TxInParameter {
-                tx_hash: "db0937db0e8a743e6e97e8cf29077af1e951b52e46f2e2c63ef12a3abaaf9052".to_string(),
-                tx_index: 1,
-                amount: Some(vec![Asset::new_from_str("lovelace", "4633697637")]),
-                address: Some("addr_test1qzjhvr7xdqmyk6x7ax84rtgs3uasqyrvglz4k08kwhw4q4jp2fnzs02hl5fhjdtw07kkxeyfac0gf9aepnpp4vv3yy2s67j7tj".to_string())
-            },
-            script_tx_in: ScriptTxInParameter {
-                script_source: Some(whisky_common::ScriptSource::ProvidedScriptSource(ProvidedScriptSource {
-                    script_cbor: "525101010023259800a518a4d136564004ae69".to_string(),
-                    language_version: whisky_common::LanguageVersion::V3
-                })),
-                datum_source: Some(DatumSource::InlineDatumSource(InlineDatumSource {
+        inputs: vec![
+            TxIn::PubKeyTxIn(PubKeyTxIn {
+                tx_in: TxInParameter {
+                    tx_hash: "db0937db0e8a743e6e97e8cf29077af1e951b52e46f2e2c63ef12a3abaaf9052".to_string(),
+                    tx_index: 0,
+                    amount: Some(vec![Asset::new_from_str("lovelace", "4633697637")]),
+                    address: Some("addr_test1qzjhvr7xdqmyk6x7ax84rtgs3uasqyrvglz4k08kwhw4q4jp2fnzs02hl5fhjdtw07kkxeyfac0gf9aepnpp4vv3yy2s67j7tj".to_string()),
+                },
+            }),
+            TxIn::ScriptTxIn(ScriptTxIn {
+                tx_in: TxInParameter {
                     tx_hash: "db0937db0e8a743e6e97e8cf29077af1e951b52e46f2e2c63ef12a3abaaf9052".to_string(),
                     tx_index: 1,
-                })),
-                redeemer: Some(Redeemer {
-                    data: "d87980".to_string(),
-                    ex_units: Budget {
-                        mem: 0,
-                        steps:0
-                    }
-                })
-            }
-        })],
+                    amount: Some(vec![Asset::new_from_str("lovelace", "4633697637")]),
+                    address: Some("addr_test1qzjhvr7xdqmyk6x7ax84rtgs3uasqyrvglz4k08kwhw4q4jp2fnzs02hl5fhjdtw07kkxeyfac0gf9aepnpp4vv3yy2s67j7tj".to_string()),
+                },
+                script_tx_in: ScriptTxInParameter {
+                    script_source: Some(whisky_common::ScriptSource::ProvidedScriptSource(ProvidedScriptSource {
+                        script_cbor: "525101010023259800a518a4d136564004ae69".to_string(),
+                        language_version: whisky_common::LanguageVersion::V3,
+                    })),
+                    datum_source: Some(DatumSource::InlineDatumSource(InlineDatumSource {
+                        tx_hash: "db0937db0e8a743e6e97e8cf29077af1e951b52e46f2e2c63ef12a3abaaf9052".to_string(),
+                        tx_index: 1,
+                    })),
+                    redeemer: Some(Redeemer {
+                        data: "d87980".to_string(),
+                        ex_units: Budget { mem: 0, steps: 0 },
+                    }),
+                },
+            }),
+        ],
         outputs: vec![
             Output {
                 address: "addr_test1qzjhvr7xdqmyk6x7ax84rtgs3uasqyrvglz4k08kwhw4q4jp2fnzs02hl5fhjdtw07kkxeyfac0gf9aepnpp4vv3yy2s67j7tj".to_string(),
@@ -46,7 +50,7 @@ fn test_from_tx_builder_body() {
                 datum: Some(Datum::Inline("d87980".to_string())),
                 reference_script: Some(whisky_common::OutputScriptSource::ProvidedScriptSource(ProvidedScriptSource {
                     script_cbor: "525101010023259800a518a4d136564004ae69".to_string(),
-                    language_version: whisky_common::LanguageVersion::V3
+                    language_version: whisky_common::LanguageVersion::V3,
                 })),
             },
             Output {
@@ -55,14 +59,30 @@ fn test_from_tx_builder_body() {
                 datum: Some(Datum::Hash("d87980".to_string())),
                 reference_script: Some(whisky_common::OutputScriptSource::ProvidedScriptSource(ProvidedScriptSource {
                     script_cbor: "525101010023259800a518a4d136564004ae69".to_string(),
-                    language_version: whisky_common::LanguageVersion::V3
+                    language_version: whisky_common::LanguageVersion::V3,
                 })),
-            }
+            },
         ],
         collaterals: vec![],
         reference_inputs: vec![],
         withdrawals: vec![],
-        mints: vec![],
+        mints: vec![
+            MintItem::ScriptMint(ScriptMint {
+                mint: MintParameter {
+                    policy_id: "bd3ae991b5aafccafe5ca70758bd36a9b2f872f57f6d3a1ffa0eb777".to_string(),
+                    asset_name: "".to_string(),
+                    amount: 1,
+                },
+                redeemer: Some(Redeemer {
+                    data: "d87980".to_string(),
+                    ex_units: Budget { mem: 0, steps: 0 },
+                }),
+                script_source: Some(ScriptSource::ProvidedScriptSource(ProvidedScriptSource {
+                    script_cbor: "5101010023259800a518a4d136564004ae69".to_string(),
+                    language_version: whisky_common::LanguageVersion::V3,
+                })),
+            }),
+        ],
         certificates: vec![],
         votes: vec![],
         change_address: "addr_test1qzjhvr7xdqmyk6x7ax84rtgs3uasqyrvglz4k08kwhw4q4jp2fnzs02hl5fhjdtw07kkxeyfac0gf9aepnpp4vv3yy2s67j7tj".to_string(),
@@ -85,13 +105,15 @@ fn test_from_tx_builder_body() {
 }
 
 #[test]
-fn test_stake_cred() {
-    let stake_credential = StakeCredential::from_bech32("stake_test1uqevw2xnsc0pvn9t9r9c7qryfqfeerchgrlm3ea2nefr9hqp8n5xl");
-    println!("Stake Credential: {:?}", stake_credential);
-}
-
-#[test]
 fn test_drep() {
     let drep = DRep::from_bech32("drep1yfaaqfzaukju6pr5wa4nhzqglv57p46cjlws424m6hhj3kg2k9vj7");
     println!("DRep: {:?}", drep);
+}
+
+#[test]
+fn test_stake_cred() {
+    let stake_credential = StakeCredential::from_bech32(
+        "stake_test1uqevw2xnsc0pvn9t9r9c7qryfqfeerchgrlm3ea2nefr9hqp8n5xl",
+    );
+    println!("Stake Credential: {:?}", stake_credential);
 }
