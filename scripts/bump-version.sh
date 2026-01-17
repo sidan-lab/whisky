087bb6217e044b6fb6f20d760d1a5cb4e1541cb8
hinsonsidan <wongkahinhinson@gmail.com>
2026-01-17 20:27:01 +0800

    chore: release

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -5,6 +5,7 @@ CARGO_TOML="packages/Cargo.toml"
 WHISKY_MACROS_CARGO_TOML="packages/whisky-macros/Cargo.toml"
 WHISKY_COMMON_CARGO_TOML="packages/whisky-common/Cargo.toml"
 WHISKY_CSL_CARGO_TOML="packages/whisky-csl/Cargo.toml"
+WHISKY_PALLAS_CARGO_TOML="packages/whisky-pallas/Cargo.toml"
 WHISKY_JS_CARGO_TOML="packages/whisky-js/Cargo.toml"
 WHISKY_PROVIDER_CARGO_TOML="packages/whisky-provider/Cargo.toml"
 WHISKY_WALLET_CARGO_TOML="packages/whisky-wallet/Cargo.toml"
@@ -41,6 +42,10 @@ sed -i '' "s/whisky-macros = { version = \"=$current_version\"/whisky-macros = {
 # Update the version in whisky-csl Cargo.toml
 sed -i '' "s/version = \"$current_version\"/version = \"$new_version\"/" "$WHISKY_CSL_CARGO_TOML"
 sed -i '' "s/whisky-common = { version = \"=$current_version\"/whisky-common = { version = \"=$new_version\"/" "$WHISKY_CSL_CARGO_TOML"
+
+# Update the version in whisky-pallas Cargo.toml
+sed -i '' "s/version = \"$current_version\"/version = \"$new_version\"/" "$WHISKY_PALLAS_CARGO_TOML"
+sed -i '' "s/whisky-csl = { version = \"=$current_version\"/whisky-csl = { version = \"=$new_version\"/" "$WHISKY_PALLAS_CARGO_TOML"
 
 # Update the version in whisky-js Cargo.toml
 sed -i '' "s/version = \"$current_version\"/version = \"$new_version\"/" "$WHISKY_JS_CARGO_TOML"
