db8e27d78931d54ee2af3c438d2c744122f45d89
hinsonsidan <wongkahinhinson@gmail.com>
2026-01-17 20:56:10 +0800

    chore: release

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -45,7 +45,7 @@ sed -i '' "s/whisky-common = { version = \"=$current_version\"/whisky-common = {
 
 # Update the version in whisky-pallas Cargo.toml
 sed -i '' "s/version = \"$current_version\"/version = \"$new_version\"/" "$WHISKY_PALLAS_CARGO_TOML"
-sed -i '' "s/whisky-csl = { version = \"=$current_version\"/whisky-csl = { version = \"=$new_version\"/" "$WHISKY_PALLAS_CARGO_TOML"
+sed -i '' "s/whisky-common = { version = \"=$current_version\"/whisky-common = { version = \"=$new_version\"/" "$WHISKY_PALLAS_CARGO_TOML"
 
 # Update the version in whisky-js Cargo.toml
 sed -i '' "s/version = \"$current_version\"/version = \"$new_version\"/" "$WHISKY_JS_CARGO_TOML"
